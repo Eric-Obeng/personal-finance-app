12fd6501f25c3bee50fb50f7b78f57dfb8decb11
Eric-Obeng <obengeric.3958@gmail.com>
2025-03-08 19:09:09 +0000

    feat: add SVG service and model with initial icons

---
+++ src/app/core/services/svg/svg.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { SvgIcon } from '../../models/svg.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SvgService {
+  private svgList: SvgIcon[] = [
+    { name: 'overview', path: 'assets/images/icon-nav-overview.svg' },
+    { name: 'transactions', path: 'assets/images/icon-nav-transactions.svg' },
+    { name: 'budget', path: 'assets/images/icon-nav-budgets.svg' },
+    { name: 'pot', path: 'assets/images/icon-nav-pots.svg' },
+    { name: 'bill', path: 'assets/images/icon-nav-recurring-bills.svg' },
+  ];
+
+  activeIcon = signal<string | null>(
+    localStorage.getItem('activeIcon') || 'overview'
+  );
+
+  get icons(): SvgIcon[] {
+    return this.svgList;
+  }
+
+  setActiveIcon(name: string): void {
+    this.activeIcon.set(name);
+    localStorage.setItem('activeIcon', name);
+  }
+
+  getActiveIcon(): string | null {
+    return this.activeIcon();
+  }
+}
