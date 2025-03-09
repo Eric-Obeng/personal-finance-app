7d0742d6c182f8f66b08b2067e06ed5713cf9888
Eric-Obeng <obengeric.3958@gmail.com>
2025-03-09 14:23:34 +0000

    updated app component with router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonModule} from 'primeng/button';
-import { NavigationsComponent } from "./core/shared/navigations/navigations.component";
+import { ButtonModule } from 'primeng/button';
+import { NavigationsComponent } from './core/shared/navigations/navigations.component';
+import { DataService } from './core/services/data/data.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ButtonModule, NavigationsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'personal-finance-app';
