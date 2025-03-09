f7033bf1002770447690c438dea453c7a62add89
Eric-Obeng <obengeric.3958@gmail.com>
2025-03-09 14:16:06 +0000

    feat: init pot summary

---
+++ src/app/core/components/pot-summary/pot-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pot-summary',
+  imports: [],
+  templateUrl: './pot-summary.component.html',
+  styleUrl: './pot-summary.component.scss'
+})
+export class PotSummaryComponent {
+
+}
