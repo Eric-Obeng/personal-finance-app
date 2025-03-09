5f81f38e3520f0784edcf6767a69fb5a1af583f7
Eric-Obeng <obengeric.3958@gmail.com>
2025-03-09 14:19:30 +0000

    feat: implements navigations component with routing and active state styling

--- src/app/core/shared/navigations/navigations.component.ts
+++ src/app/core/shared/navigations/navigations.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { SvgService } from '../../services/svg/svg.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigations',
-  imports: [MatIconModule, CommonModule],
+  imports: [MatIconModule, CommonModule, RouterModule],
   templateUrl: './navigations.component.html',
   styleUrl: './navigations.component.scss',
 })
