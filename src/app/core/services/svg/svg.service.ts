1ac832fcb80bbca2a86953aee8e2ccb0ceb1b729
Eric-Obeng <obengeric.3958@gmail.com>
2025-03-10 08:12:09 +0000

    feat: enhance SvgService with new icons and methods for improved SVG handling

--- src/app/core/services/svg/svg.service.ts
+++ src/app/core/services/svg/svg.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
 import { SvgIcon } from '../../models/svg.model';
 
@@ -7,12 +7,21 @@ import { SvgIcon } from '../../models/svg.model';
   providedIn: 'root',
 })
 export class SvgService {
+  sanitizer = inject(DomSanitizer);
+
   private svgList: SvgIcon[] = [
     { name: 'overview', path: 'assets/images/icon-nav-overview.svg' },
     { name: 'transactions', path: 'assets/images/icon-nav-transactions.svg' },
     { name: 'budgets', path: 'assets/images/icon-nav-budgets.svg' },
     { name: 'pots', path: 'assets/images/icon-nav-pots.svg' },
-    { name: 'recurring-bills', path: 'assets/images/icon-nav-recurring-bills.svg' },
+    {
+      name: 'recurring-bills',
+      path: 'assets/images/icon-nav-recurring-bills.svg',
+    },
+    {
+      name: 'right-arrow',
+      path: 'assets/images/icon-caret-right.svg',
+    },
   ];
 
   activeIcon = signal<string | null>(
@@ -23,6 +32,10 @@ export class SvgService {
     return this.svgList;
   }
 
+  get navigationIcons(): SvgIcon[] {
+    return this.svgList.slice(0, 5);
+  }
+
   setActiveIcon(name: string): void {
     this.activeIcon.set(name);
     localStorage.setItem('activeIcon', name);
@@ -30,5 +43,10 @@ export class SvgService {
 
   getActiveIcon(): string | null {
     return this.activeIcon();
+  }
+
+  getSvgContent(path: string): SafeHtml {
+    const svgContent = `<img src="${path}" alt="icon">`;
+    return this.sanitizer.bypassSecurityTrustHtml(svgContent);
   }
 }
